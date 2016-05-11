3bb90edadb3c40368a151f14da4151deefd5a3b9
watix <bccollado@gmail.com>
2016-05-11 12:24:27 +0200

    Pruebas

--- ProyectoDds/src/Game.java
+++ ProyectoDds/src/Game.java
@@ -1,4 +1,56 @@
+import java.awt.Canvas;
+import java.awt.Dimension;
 
-public class Game {
-int testing = 1;
+import javax.swing.JFrame;
+
+public class Game extends Canvas implements Runnable {
+	private static final long serialVersionUID = 1L;
+	public static int width = 300;
+	public static int height = width / 16 * 9;
+	public static int scale = 3;
+
+	private Thread thread;
+	private boolean running = false;
+	private JFrame frame;
+	
+	
+	public Game (){
+		Dimension size = new Dimension(width * scale , height * scale);
+		setPreferredSize(size);
+		frame = new JFrame();
+	}
+	public synchronized void start() {
+		thread = new Thread(this, "Display");
+		thread.start();
+		running = true;
+	}
+
+	public synchronized void stop() {
+		running = false;
+		try {
+			thread.join();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void run (){
+		while (running){
+			System.out.println("Running...");
+		}		
+	}
+	
+	public static void main(String[] args){
+		
+		Game game = new Game();
+		game.frame.setResizable(false);
+		game.frame.setTitle("AWesoME");
+		game.frame.add(game);
+		game.frame.pack();
+		game.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		game.frame.setLocationRelativeTo(null);
+		game.frame.setVisible(true);
+		game.start();
+		
+	}
 }

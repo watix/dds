95822e6a8eec3472753fc520dc43312246b90f6c
watix <bccollado@gmail.com>
2016-05-16 11:15:14 +0200

    prueba4

--- ProyectoDds/src/Screen.java
+++ ProyectoDds/src/Screen.java
@@ -1,34 +1,41 @@
+import java.util.Random;
 
 public class Screen {
 	private int width, height;
 	public int[] pixels;
-	int xtime = 50, ytime = 50;
-	int counter = 0;
-	
-	public Screen (int width, int height){
-		
+	public int[] tiles = new int[64 * 64];
+
+	public Random random = new Random();
+
+	public Screen(int width, int height) {
+
 		this.width = width;
 		this.height = height;
-		pixels = new int[width*height];
-	}
-	public void clear(){
-		for(int i = 0; i < pixels.length; i++){
-			pixels[i]= 0;			
+		pixels = new int[width * height];
+
+		for (int i = 0; i < 64 * 64; i++) {
+
+			tiles[i] = random.nextInt(0xffffff);
+
 		}
 	}
-	
-	public void render(){
-		counter++;
-	if (counter % 10 == 0)	xtime++;
-	if (counter % 10 == 0) ytime++;
-	
-		for(int y = 0; y < height; y++){
-			if(ytime < 0 || ytime >= height) break;
-			for(int x =0; x < width; x++){
-				if(xtime < 0 || xtime >= width) break;
-				pixels[xtime + ytime * width] = 0XFF00FF;
+
+	public void clear() {
+		for (int i = 0; i < pixels.length; i++) {
+			pixels[i] = 0;
+		}
+	}
+
+	public void render() {
+
+		for (int y = 0; y < height; y++) {
+			if (y < 0 || y >= height) break;
+			for (int x = 0; x < width; x++) {
+				if (x < 0 || x >= width) break;
+				int tileIndex = (x >> 4) + (y >> 4) * 64;
+				pixels[x + y * width] = tiles[tileIndex];
 			}
 		}
 	}
-	
+
 }

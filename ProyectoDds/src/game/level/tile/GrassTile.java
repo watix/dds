f470a7815667a59ab0aafaf5e66c22ab5c05519c
watix <bccollado@gmail.com>
2016-05-16 18:12:49 +0200

    rendering

---
+++ ProyectoDds/src/game/level/tile/GrassTile.java
@@ -0,0 +1,15 @@
+package game.level.tile;
+
+import game.graphics.Screen;
+import game.graphics.Sprite;
+
+public class GrassTile extends Tile {
+
+	public GrassTile(Sprite sprite) {
+		super(sprite);
+	}
+	public void render(int x, int y, Screen screen) {
+		//Do render stuff here!
+	}
+
+}
